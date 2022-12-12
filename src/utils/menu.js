23746b9aa0ad62d2d5d5c5834a50171cf58ad53d
cangyu1993 <1765244720@qq.com>
2022-12-12 10:44:19 +0800

    first commit

---
+++ src/utils/menu.js
@@ -0,0 +1,68 @@
+export const menu = [
+    {
+        nemuNumber: '1',
+        nemuName: '账户与缴费',
+        children: [
+            {
+                nemuNumber: '1',
+                nemuName: '账户查询',
+            },
+            {
+                nemuNumber: '2',
+                nemuName: '个人信息更新',
+            },
+            {
+                nemuNumber: '3',
+                nemuName: '对公预约开户',
+            },
+            {
+                nemuNumber: '4',
+                nemuName: '公积金查询',
+            },
+        ]
+    },
+    {
+        nemuNumber: '2',
+        nemuName: '我要开卡',
+        children: [
+            {
+                nemuNumber: '1',
+                nemuName: '预约开户',
+            },
+            {
+                nemuNumber: '2',
+                nemuName: '数字卡预约',
+            },
+            {
+                nemuNumber: '3',
+                nemuName: '社保卡申请',
+            },
+            {
+                nemuNumber: '4',
+                nemuName: '信用卡申请',
+            },
+        ]
+    },
+    {
+        nemuNumber: '3',
+        nemuName: '我要理财',
+        children: [
+            {
+                nemuNumber: '1',
+                nemuName: '理财',
+            },
+            {
+                nemuNumber: '2',
+                nemuName: '基金',
+            },
+            {
+                nemuNumber: '3',
+                nemuName: '大额存单',
+            },
+            {
+                nemuNumber: '4',
+                nemuName: '特色存款',
+            },
+        ]
+    }
+]
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,87 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <draggable
+    class="wrapper"
+    :list="list"
+    @update="datadragEnd"
+    v-bind="dragOptions"
+  >
+    <transition-group type="transition" name="flip-list">
+      <div v-for="(item, index) in list" :key="index" class="item">
+        <draggable
+          class="wrapper"
+          :list="item"
+          @update="datadragEndSub(index, $event)"
+          v-bind="dragOptions"
+        >
+          <transition-group>
+            <p v-for="(item2, index2) in item" :key="index2">{{ item2 }}</p>
+          </transition-group>
+        </draggable>
+      </div>
+    </transition-group>
+  </draggable>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import draggable from "vuedraggable";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name: "HelloWorld",
+  components: { draggable },
+  data() {
+    return {
+      list: [
+        [1, 2, 3, 4],
+        [7, 8, 5, 6],
+        [9, 10, 5, 6],
+        [11, 12, 3, 5],
+        [13, 14, 4, 6],
+        [15, 16, 3, 6],
+      ],
+      dragOptions: {
+        animation: 200,
+        group: "description",
+        disabled: false,
+        ghostClass: "ghost",
+      },
+    };
+  },
+  methods: {
+    datadragEnd(evt) {
+      console.log(this.list);
+    },
+    datadragEndSub(index, evt) {
+      console.log(this.list);
+    },
+  },
+};
+</script>
+<style scoped lang="scss">
+ul {
+  list-style: none;
+}
+.flip-list-move {
+  transition: transform 0.5s;
+}
+.no-move {
+  transition: transform 0s;
+}
+.wrapper {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+
+  .item {
+    margin: 20px;
+    width: 300px;
+    background-color: #42b983;
+    color: #ffffff;
+    p {
+      margin: 5px;
+      padding: 10px;
+      height: 20px;
+      background: lightcoral;
+    }
   }
 }
-</script>
+</style>
